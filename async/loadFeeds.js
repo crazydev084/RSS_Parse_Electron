1e9f4a8ffeec332147a62261193b48aef3a68877
crazydev084 <crazydev084@gmail.com>
2025-06-25 04:01:04 +0900

    set timeInterval

--- async/loadFeeds.js
+++ async/loadFeeds.js
@@ -9,7 +9,7 @@ module.exports = (feeds) => {
   )
   .then(feeds => {
     const app = document.getElementById('app');
-
+    app.innerHTML = '';
     feeds.forEach((feed, index) => {
       app.appendChild(FeedDigest(feed, index));
     });
